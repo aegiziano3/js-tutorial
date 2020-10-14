875631b2b4584a07f30a6ee631ab6f405f60a2c4
ariana <arianaegiziano@gmail.com>
2020-10-14 12:29:26 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+        // this is a javascript code
+        console.log('Hello World');
+   let name = 'Ariana';
+   console.log(name);
+   //Cannot use a reserved keyword
+//    Name variables should be meaningful
+// Cannot start with a number (1name)
+// cannot contain a space or hyphen (-)
+// Case-sesitive
+// 
+let firstName = 'Ariana';
+// string Literal
+let age = 30;
+// Number Literal
+let isApproved = false;
+// Boolean Literal
+let firstName = underfined;
+let selectedColor = null;
+
+let lastName = 'Egiziano'
+
+let interstRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+let person = {
+    name: 'Ariana',
+    age: 23
+};
+ 
+// Dot Notation
+person.name = 'Jess';
+// Bracket Notation
+let selection = 'name';
+person ['selection'] = 'Mary';
+console.log(person.name)
+
+// Arrays
+
